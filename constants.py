9bb99d46eaab545ecaab9b3296456bf734f1eac2
Agnieszka Pawlaczek <agnieszkapawlaczek7@gmail.com>
2024-11-04 14:24:13 +0100

    Dodanie nowego QGIS-feed #17

---
+++ constants.py
@@ -0,0 +1,15 @@
+INDUSTRIES = {
+    "999":"Nie wybrano",
+    "e":"Energetyka/OZE",
+    "u":"Urząd",
+    "td":"Transport/Drogi",
+    "pg":"Planowanie/Geodezja",
+    "wk":"WodKan",
+    "s":"Środowisko",
+    "rl":"Rolnictwo/Leśnictwo",
+    "tk":"Telkom",
+    "edu":"Edukacja",
+    "i":"Inne",
+    "it":"IT",
+    "n":"Nieruchomości"
+}
