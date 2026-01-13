54e4e3e4c978b18ef60e39f80574974fcf661e25
Agnieszka Pawlaczek <157118546+apawlaczek@users.noreply.github.com>
2026-01-13 16:26:17 +0100

    Zmiana requests na QgsNetworkAccessManager w NmtAPI #39

--- nmt_api.py
+++ nmt_api.py
@@ -1,21 +1,37 @@
-import requests
+from qgis.core import QgsNetworkAccessManager
+from PyQt5.QtCore import QUrl, QUrlQuery, QEventLoop
+from PyQt5.QtNetwork import QNetworkRequest, QNetworkReply
 
 
 class NmtAPI:
 
     URL = "http://services.gugik.gov.pl/nmt/"
 
+    @staticmethod
     def getRequest(PARAMS):
-        try:
-            r = requests.get(url=NmtAPI.URL, params=PARAMS)
-        except requests.exceptions.ConnectionError:
-            return None
-        r_txt = r.text
-        if r.status_code == 200:
-            return r_txt
+        url = QUrl(NmtAPI.URL)
+        query = QUrlQuery()
+
+        for key, value in PARAMS.items():
+            query.addQueryItem(str(key), str(value))
+
+        url.setQuery(query)
+        request = QNetworkRequest(url)
+        manager = QgsNetworkAccessManager.instance()
+        reply = manager.get(request)
+        loop = QEventLoop()
+        reply.finished.connect(loop.quit)
+        loop.exec_()
+
+        if reply.error() == QNetworkReply.NoError:
+            data = reply.readAll().data().decode("utf-8")
+            reply.deleteLater()
+            return data
         else:
+            reply.deleteLater()
             return None
 
+    @staticmethod
     def getHbyXY(x, y):
         PARAMS = {'request': "GetHbyXY", 'x': x, 'y': y}
         return NmtAPI.getRequest(PARAMS)
