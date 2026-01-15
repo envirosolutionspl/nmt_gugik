cea6ce99e2d0f5e92a67ce79f14c5b5cdec902f4
Agnieszka Pawlaczek <157118546+apawlaczek@users.noreply.github.com>
2026-01-15 13:06:34 +0100

    Zmiany dla Qt6 - QNetworkReply i QNetworkRequest #39

--- nmt_api.py
+++ nmt_api.py
@@ -3,6 +3,7 @@ from qgis.PyQt.QtCore import QUrl, QUrlQuery, QEventLoop
 from qgis.PyQt.QtNetwork import QNetworkRequest, QNetworkReply
 from qgis.PyQt.QtWidgets import QDialog
 from .constants import NMT_SERVICE_URL
+from . import PLUGIN_NAME, PLUGIN_VERSION
 
 if not hasattr(QEventLoop, 'exec'):
     QEventLoop.exec = QEventLoop.exec_
@@ -24,13 +25,25 @@ class NmtAPI:
 
         url.setQuery(query)
         request = QNetworkRequest(url)
+        if hasattr(QNetworkRequest, 'KnownHeaders'):
+            ua_header = QNetworkRequest.KnownHeaders.UserAgentHeader
+        else:
+            ua_header = QNetworkRequest.UserAgentHeader
+        request.setHeader(
+             ua_header, f"QGIS-Plugin-{PLUGIN_NAME}"
+        )
+        
         manager = QgsNetworkAccessManager.instance()
         reply = manager.get(request)
         loop = QEventLoop()
         reply.finished.connect(loop.quit)
         loop.exec()
-
-        if reply.error() == QNetworkReply.NoError:
+        error_val = reply.error()
+        if hasattr(QNetworkReply, 'NetworkError'):
+            no_err = QNetworkReply.NetworkError.NoError  # Qt6
+        else:
+            no_err = QNetworkReply.NoError  # Qt5
+        if error_val == no_err:
             data = reply.readAll().data().decode("utf-8")
             reply.deleteLater()
             return data
