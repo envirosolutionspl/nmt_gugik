8c1b50087a5783881cdf4f8f491dffe8486a13f0
Agnieszka Pawlaczek <157118546+apawlaczek@users.noreply.github.com>
2026-01-20 09:34:50 +0100

    Rozbicie funkcji getRequest #43

--- nmt_api.py
+++ nmt_api.py
@@ -16,45 +16,61 @@ class NmtAPI:
     URL = NMT_SERVICE_URL
 
     @staticmethod
-    def getRequest(PARAMS):
+    def buildUrl(params):
         url = QUrl(NmtAPI.URL)
         query = QUrlQuery()
-
-        for key, value in PARAMS.items():
+        for key, value in params.items():
             query.addQueryItem(str(key), str(value))
-
         url.setQuery(query)
+        return url
+
+    @staticmethod
+    def buildRequest(url):
         request = QNetworkRequest(url)
         if hasattr(QNetworkRequest, 'KnownHeaders'):
-            ua_header = QNetworkRequest.KnownHeaders.UserAgentHeader
+            ua_header = QNetworkRequest.KnownHeaders.UserAgentHeader  # Qt6
         else:
-            ua_header = QNetworkRequest.UserAgentHeader
+            ua_header = QNetworkRequest.UserAgentHeader  # Qt5
         request.setHeader(
-             ua_header, f"QGIS-Plugin-{PLUGIN_NAME}"
+            ua_header, f"QGIS-Plugin-{PLUGIN_NAME}"
         )
-        
+        return request
+
+    @staticmethod
+    def sendRequest(request):
         manager = QgsNetworkAccessManager.instance()
         reply = manager.get(request)
         loop = QEventLoop()
         reply.finished.connect(loop.quit)
         loop.exec()
+        return reply
+
+    @staticmethod
+    def isSuccess(reply):
         error_val = reply.error()
         if hasattr(QNetworkReply, 'NetworkError'):
             no_err = QNetworkReply.NetworkError.NoError  # Qt6
         else:
             no_err = QNetworkReply.NoError  # Qt5
-        if error_val == no_err:
-            data = reply.readAll().data().decode("utf-8")
-            reply.deleteLater()
-            return data
-        else:
-            reply.deleteLater()
-            return None
+        return error_val == no_err
+
+    @staticmethod
+    def readReply(reply):
+        data = reply.readAll().data().decode("utf-8")
+        reply.deleteLater()
+        return data
+
+    @staticmethod
+    def getRequest(params):
+        url = NmtAPI.buildUrl(params)
+        request = NmtAPI.buildRequest(url)
+        reply = NmtAPI.sendRequest(request)
+        if NmtAPI.isSuccess(reply):
+            return NmtAPI.readReply(reply)
+        reply.deleteLater()
+        return None
 
     @staticmethod
     def getHbyXY(x, y):
-        PARAMS = {'request': "GetHbyXY", 'x': x, 'y': y}
-        return NmtAPI.getRequest(PARAMS)
-
-
-
+        params = {'request': "GetHbyXY", 'x': x, 'y': y}
+        return NmtAPI.getRequest(params)
